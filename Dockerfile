365f461af89b88e7ed42d1b2f1f36911f9a2bd07
Anita Caron <anitacaron@alumni.usp.br>
2025-06-15 17:17:05 -0300

    simple dockerfile to have obo dashboard independent of ODK

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM obolibrary/robot:latest
+
+WORKDIR /tools
+
+# Install system dependencies
+RUN apt-get update && \
+    apt-get install -y git \
+        python3 \
+        python3-pip \
+        python3-venv \
+        python-is-python3 \
+        jq && \
+    rm -rf /var/lib/apt/lists/*
+
+
+# Copy the OBO Dashboard source code
+COPY . .
+RUN python3 -m pip install -r requirements.txt --break-system-packages
+
--- odk.sh
+++ odk.sh
@@ -9,9 +9,13 @@
 # we therefore map the whole repo (../..) to a docker volume.
 #
 # See README-editors.md for more details.
+# docker run -e ROBOT_JAVA_ARGS='-Xmx48G' -e JAVA_OPTS='-Xmx48G' \
+#   -v $PWD/dashboard:/tools/OBO-Dashboard/dashboard \
+#   -v $PWD/dashboard-config.yml:/tools/OBO-Dashboard/dashboard-config.yml \
+#   -v $PWD/ontologies:/tools/OBO-Dashboard/build/ontologies \
+#   -v $PWD/sparql:/tools/OBO-Dashboard/sparql \
+#   -w /work --rm -ti obolibrary/odkfull "$@"
+
+# This is a wrapper for the OBO Dashboard Dockerfile.
 docker run -e ROBOT_JAVA_ARGS='-Xmx48G' -e JAVA_OPTS='-Xmx48G' \
-  -v $PWD/dashboard:/tools/OBO-Dashboard/dashboard \
-  -v $PWD/dashboard-config.yml:/tools/OBO-Dashboard/dashboard-config.yml \
-  -v $PWD/ontologies:/tools/OBO-Dashboard/build/ontologies \
-  -v $PWD/sparql:/tools/OBO-Dashboard/sparql \
-  -w /work --rm -ti obolibrary/odkfull "$@"
+  -w /tools --rm -ti docker.io/library/obo-dashboard "$@"
